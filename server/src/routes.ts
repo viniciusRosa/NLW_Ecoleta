abc0f2b769d7cea3e850a2e958df36d92370b5b1
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-06-05 02:26:50 -0300

    point post route created

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,56 @@
 import express, { response } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    response.json({ message: 'hello' });
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        };
+    })
+
+    response.json(serializedItems);
 });
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const ids = await knex('points').insert({
+        image: 'fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    })
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id: ids[0]
+        };
+    })
+
+    await knex('point_items').insert(pointItems);
+
+    return response.json({ message: true });
+})
+
 
 export default routes;
