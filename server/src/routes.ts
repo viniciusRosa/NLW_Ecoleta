4da2aac7545a96b5378673c6455f1a1a4a06daac
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-07-26 20:01:07 -0300

    project complete

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,16 +1,24 @@
 import express from 'express';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import PointsControllers from './controllers/PointsControllers';
 import ItemControllers from './controllers/ItemControllers';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsControllers();
 const itemController = new ItemControllers()
 
 routes.get('/items', itemController.index);
 
-routes.post('/points', pointsController.create)
-routes.get('/points', pointsController.index)
-routes.get('/points/:id', pointsController.show)
+routes.post('/points', upload.single('image'), pointsController.create); 
 
 
-export default routes;
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
+export default routes; 
