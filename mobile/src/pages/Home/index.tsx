5284032b7d25539d7d1806a9ddadf4c2c25d7ba1
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-07-26 15:30:33 -0300

    application complete

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,107 +1,131 @@
-import React from 'react';
-import { View, Image, StyleSheet, Text, ImageBackground } from 'react-native';
+import React, {useState} from 'react';
+import { View, Image, StyleSheet, Text, ImageBackground, TextInput, KeyboardAvoidingView } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler'
 import { Feather as Icon } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
 
+  const [uf, setUf] = useState <string> ('')
+  const [city, setCity] = useState <string> ('')
+
   const navigation = useNavigation();
 
   function handleNavigateToPoints() {
-    navigation.navigate('Points')
+    navigation.navigate('Points', {
+      uf,
+      city
+    })
   }
-    return (
+  return (
     <ImageBackground source={require('../../assets/home-background.png')} style={styles.container}>
-        <View style={styles.main}>
-            <Image source={require('../../assets/logo.png')} />
-            <Text style={styles.title}> Seu marketplace de coleta de resíduos</Text>
-            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-        </View>
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}> Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+      </View>
 
-        <View style={styles.footer}>
-            <RectButton style={styles.button} onPress={handleNavigateToPoints}> 
-                <View style={styles.buttonIcon}>
-                    <Text> 
-                        <Icon name='arrow-right' color='#fff' size={24}></Icon>
-                    </Text>
-                </View>
-                <Text style={styles.buttonText}>
-                    Entrar
+      <View style={styles.footer}>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite a UF'
+          maxLength={2}
+          autoCapitalize='characters'
+          autoCorrect={false}
+          value={uf}
+          onChangeText={setUf}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder='Digite a cidade'
+          autoCorrect={false}
+          value={city}
+          onChangeText={setCity}
+        />
+
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name='arrow-right' color='#fff' size={24}></Icon>
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
                 </Text>
-            </RectButton>
-        </View>
+        </RectButton>
+      </View>
     </ImageBackground>
     )
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 32,
-    },
-  
-    main: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-  
-    title: {
-      color: '#322153',
-      fontSize: 26,
-      fontFamily: 'Ubuntu_700Bold',
-      maxWidth: 200,
-      marginTop: 48,
-    },
-  
-    description: {
-      color: '#6C6C80',
-      fontSize: 16,
-      marginTop: 16,
-      fontFamily: 'Roboto_400Regular',
-      maxWidth: 260,
-      lineHeight: 24,
-    },
-  
-    footer: {},
-  
-    select: {},
-  
-    input: {
-      height: 60,
-      backgroundColor: '#FFF',
-      borderRadius: 10,
-      marginBottom: 8,
-      paddingHorizontal: 24,
-      fontSize: 16,
-    },
-  
-    button: {
-      backgroundColor: '#34CB79',
-      height: 60,
-      flexDirection: 'row',
-      borderRadius: 10,
-      overflow: 'hidden',
-      alignItems: 'center',
-      marginTop: 8,
-    },
-  
-    buttonIcon: {
-      height: 60,
-      width: 60,
-      backgroundColor: 'rgba(0, 0, 0, 0.1)',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-  
-    buttonText: {
-      flex: 1,
-      justifyContent: 'center',
-      textAlign: 'center',
-      color: '#FFF',
-      fontFamily: 'Roboto_500Medium',
-      fontSize: 16,
-    }
-  });
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 26,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 200,
+    marginTop: 32,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 8,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  }
+});
 
 export default Home;
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { View, StyleSheet, TouchableOpacity, Text, ScrollView, Image, Alert } from 'react-native';
 import Constants from 'expo-constants'
 import { Feather as Icon } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps'
 import { SvgUri } from 'react-native-svg'
 import api from '../../services/api'
@@ -23,7 +23,15 @@ interface point {
     
 }
 
+interface Params {
+    uf: string;
+    city: string;
+}
+
 const Points = () => {
+
+    const route = useRoute()
+    const routeParams = route.params as Params;
 
     const [items, setItems] = useState <items []> ([]);
     const [points, setPoints] = useState <point []> ([])
@@ -63,14 +71,14 @@ const Points = () => {
     useEffect(() => {
         api.get('points', {
             params: {
-                city: 'osorio',
-                uf: 'RS',
-                items: [3, 6]
+                city: routeParams.city,
+                uf: routeParams.uf,
+                items: selectedItems
             }
         }).then(response => {
             setPoints(response.data)
         })
-    }, [])
+    }, [selectedItems])
 
 
     function handleNavigateGoBack() {
